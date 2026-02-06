334ba304bd108efbbd8b5059c55e47c79ac9ed56
Walid <walid_fekry@hotmail.com>
2026-02-06 13:31:29 +0200

    Refactor About activity: enable ViewBinding and update layout to ConstraintLayout

--- app/src/main/java/com/walid/MuslimRoad/About.java
+++ app/src/main/java/com/walid/MuslimRoad/About.java
@@ -1,135 +1,91 @@
 package com.walid.MuslimRoad;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.net.Uri;
-import android.os.Handler;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
-import guy4444.smartrate.SmartRate;
+import androidx.appcompat.app.AppCompatActivity;
 
-import net.steamcrafted.materialiconlib.MaterialIconView;
+import com.walid.MuslimRoad.databinding.ActivityAboutBinding;
+
 
 public class About extends AppCompatActivity {
-MaterialIconView gmail,facebooks,share,star,googleplays;
-TextView em,fb,sh,gooo,ra;
+
+    private ActivityAboutBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
-        gmail=(MaterialIconView)findViewById(R.id.gmail);
-        facebooks=(MaterialIconView)findViewById(R.id.facebooks);
-        share=(MaterialIconView)findViewById(R.id.share);
-        star=(MaterialIconView)findViewById(R.id.star);
-        googleplays=(MaterialIconView)findViewById(R.id.googleplays);
-        em=(TextView)findViewById(R.id.em);
-        fb=(TextView)findViewById(R.id.fb);
-        sh=(TextView)findViewById(R.id.sh);
-        gooo=(TextView)findViewById(R.id.gooo);
-        ra=(TextView)findViewById(R.id.ra);
+        binding = ActivityAboutBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        setupClicks();
+    }
 
-        em.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
-                "mailto","prowalidfekry@gmail.com", null));
+    private void setupClicks() {
+
+        // Gmail
+        ViewClick(this::sendEmail,
+                binding.gmail,
+                binding.em
+        );
+
+        // Facebook
+        ViewClick(() -> openUrl("https://www.facebook.com/Waleed.Fikri"),
+                binding.facebooks,
+                binding.fb
+        );
+
+        // Share App
+        ViewClick(() -> shareApp(),
+                binding.share,
+                binding.sh
+        );
+
+        // Rate App
+        ViewClick(() -> openUrl("https://t.co/IuFGow3uyK"),
+                binding.star,
+                binding.ra
+        );
+
+        // More Apps
+        ViewClick(() -> openUrl("https://t.co/US34fsUZeW"),
+                binding.googleplays,
+                binding.gooo
+        );
+    }
+
+    /* ---------------- Actions ---------------- */
+
+    private void sendEmail() {
+        Intent emailIntent = new Intent(
+                Intent.ACTION_SENDTO,
+                Uri.fromParts("mailto", "prowalidfekry@gmail.com", null)
+        );
         emailIntent.putExtra(Intent.EXTRA_SUBJECT, "رسالة إلى مبرمج تطبيق المسلم 📲");
-        emailIntent.putExtra(Intent.EXTRA_TEXT, "");
-        startActivity(Intent.createChooser(emailIntent, "Send email..."));
-
+        startActivity(Intent.createChooser(emailIntent, "إرسال بريد"));
     }
-});
 
-        gmail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
-                        "mailto","prowalidfekry@gmail.com", null));
-                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "رسالة إلى مبرمج تطبيق المسلم 📲");
-                emailIntent.putExtra(Intent.EXTRA_TEXT, "");
-                startActivity(Intent.createChooser(emailIntent, "Send email..."));
-
-            }
-        });
-fb.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/Waleed.Fikri")));
-
-    }
-});
-        facebooks.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/Waleed.Fikri")));
-
-            }
-        });
-sh.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        Intent sendIntent = new Intent();
-        sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_SUBJECT," طريق المسلم - MuslimRoad");
-        sendIntent.putExtra(Intent.EXTRA_TEXT,"\n" +
-                "هذا التطبيق هو إصدار مجاني لمساعدتك في شهر رمضان و يحتوي على العديد من الأذكار و المساعدات الرمضانية لإدارة وقتك بشكل صحيح و  يضم أذكار الصباح و المساء مكتوبة و أذكار المسلم بدون إنترنت .\n \n" +
-                "تفضل رابط التطبيق 👇  https://t.co/IuFGow3uyK\n");
+    private void shareApp() {
+        Intent sendIntent = new Intent(Intent.ACTION_SEND);
         sendIntent.setType("text/plain");
-        startActivity(Intent.createChooser(sendIntent,"مشاركه تطبيق طريق المسلم - MuslimRoad مع الاصدقاء:"));
-
-
+        sendIntent.putExtra(Intent.EXTRA_SUBJECT, "طريق المسلم - MuslimRoad");
+        sendIntent.putExtra(Intent.EXTRA_TEXT,
+                "هذا التطبيق إصدار مجاني لمساعدتك في شهر رمضان ويحتوي على العديد من الأذكار.\n\n" +
+                        "رابط التطبيق 👇\nhttps://t.co/IuFGow3uyK"
+        );
+        startActivity(Intent.createChooser(sendIntent, "مشاركة التطبيق"));
     }
-});
 
-        share.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent sendIntent = new Intent();
-                sendIntent.setAction(Intent.ACTION_SEND);
-                sendIntent.putExtra(Intent.EXTRA_SUBJECT," طريق المسلم- MuslimRoad");
-                sendIntent.putExtra(Intent.EXTRA_TEXT,"\n" +
-                        "هذا التطبيق هو إصدار مجاني لمساعدتك في شهر رمضان و يحتوي على العديد من الأذكار و المساعدات الرمضانية لإدارة وقتك بشكل صحيح و  يضم أذكار الصباح و المساء مكتوبة و أذكار المسلم بدون إنترنت .\n \n" +
-                        "تفضل رابط التطبيق 👇  https://t.co/IuFGow3uyK\n");
-                sendIntent.setType("text/plain");
-                startActivity(Intent.createChooser(sendIntent,"مشاركه تطبيق طريق المسلم - MuslimRoad مع الاصدقاء:"));
-
-            }
-        });
-
-ra.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.co/IuFGow3uyK" )));
-
+    private void openUrl(String url) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
     }
-});
-        star.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.co/IuFGow3uyK" )));
+    /* ---------------- Helper ---------------- */
 
-            }
-        });
-
-        googleplays.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.co/US34fsUZeW" )));
-
-            }
-        });
-
-        gooo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.co/US34fsUZeW" )));
-            }
-        });
-
+    private void ViewClick(Runnable action, android.view.View... views) {
+        for (android.view.View view : views) {
+            view.setOnClickListener(v -> action.run());
+        }
     }
 }
