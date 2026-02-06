9203598900db8f3e9a9dc35ca52e143a7d05875e
Walid <walid_fekry@hotmail.com>
2026-02-07 01:13:35 +0200

    Refactor Splash activity and reduce timeout

--- app/src/main/java/com/walid/MuslimRoad/Splash.java
+++ app/src/main/java/com/walid/MuslimRoad/Splash.java
@@ -1,40 +1,26 @@
 package com.walid.MuslimRoad;
 
 import android.content.Intent;
-import android.os.Handler;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.MobileAds;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Splash extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 2500;
+    private static final int SPLASH_TIME_OUT = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature( Window.FEATURE_NO_TITLE );
-        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN,
-       WindowManager.LayoutParams.FLAG_FULLSCREEN );
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
-
-
-
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(Splash.this, MainActivity.class);
-                startActivity(i);
-                // Prepare an Interstitial Ad Listener
-                // close this activity
-                finish();
-            }
+        new Handler().postDelayed(() -> {
+            Intent i = new Intent(Splash.this, MainActivity.class);
+            startActivity(i);
+            finish();
         }, SPLASH_TIME_OUT);
     }
 
